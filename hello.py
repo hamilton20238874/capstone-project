e1d1beba041f1ffd52f445afc61b472da4af56da
hamilton20238874 <121912144+hamilton20238874@users.noreply.github.com>
2023-01-25 15:14:59 +0000

    mmmm

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ class Student():
     def __init__(self, student_name, student_contact):
         self.student_name = student_name
         self.student_contact = student_contact
-        schedule = []
-        interest_list = []
+        #schedule = []
+        #interest_list = []
 
     def __str__(self):
         return f"This student's name is {self.student_name} and their contact is {self.student_contact}"
@@ -15,39 +15,39 @@ class Class():
         self.class_topic = class_topic
 
 class Student_Class(Class):
-  def __init__(self, class_name, class_topic, class_level):
-    self.class_level = class_level
-    super().__init__(class_name, class_topic)
+    def __init__(self, class_name, class_topic, class_level):
+        self.class_level = class_level
+        super().__init__(class_name, class_topic)
 
 class Interest():
-      def __init__(self, interest_name, interest_topic):
-          self.interest_name = interest_name
-          self.interest_topic = interest_topic
+    def __init__(self, interest_name, interest_topic):
+        self.interest_name = interest_name
+        self.interest_topic = interest_topic
 
 def compare(schedule1, schedule2, interest1, interest2):
-  counter = 0
-  x = 0
-  for i in schedule1:
-    if i.class_name == schedule2[x].class_name:
-      if i.class_level == schedule2[x].class_level:
-        counter += 1
-        x += 1
-    else:
-      x += 1
+    counter = 0
+    x = 0
+    for i in schedule1:
+        if i.class_name == schedule2[x].class_name:
+            if i.class_level == schedule2[x].class_level:
+                counter += 1
+                x += 1
+        else:
+            x += 1
 
-  if len(interest1) >= len(interest2):
-    for i in interest1:
-      for j in interest2:
-        if i.interest_name == j.interest_name:
-          counter += 3
-      
-  if len(interest2) > len(interest1):
-    for i in interest2:
-      for j in interest1:
-        if i.interest_name == j.interest_name:
-          counter += 3
+    if len(interest1) >= len(interest2):
+        for i in interest1:
+            for j in interest2:
+                if i.interest_name == j.interest_name:
+                    counter += 3
+        
+    if len(interest2) > len(interest1):
+        for i in interest2:
+            for j in interest1:
+                if i.interest_name == j.interest_name:
+                    counter += 3
 
-  return counter
+    return counter
 
 #testing
 
